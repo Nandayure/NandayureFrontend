3b67306e87295e562aa0bbc80b4cfbf491bf7c27
José Pablo <suarezgomezjosepablo03@gmail.com>
2024-10-17 19:37:28 -0600

    Refactor StudiesCategorySchema to add validation for required fields

--- src/schemas/system-configuration/StudiesCategorySchema.ts
+++ src/schemas/system-configuration/StudiesCategorySchema.ts
@@ -1,11 +1,11 @@
 import { z } from 'zod';
 
 export const StudiesCategorySchema = z.object({
-  id: z.string(),
-  description: z.string(),
-  weight: z.string(),
-  Dedication: z.string(),
-  Restriction: z.string(),
+  id: z.string().min(1, 'El ID es requerido'),
+  description: z.string().min(1, 'El campo es requerido'),
+  weight: z.string().min(1, 'El campo es requerido'),
+  Dedication: z.string().min(1, 'El campo es requerido'),
+  Restriction: z.string().min(1, 'El campo es requerido'),
 });
 
 export const UpdateStudiesCategorySchema = z.object({

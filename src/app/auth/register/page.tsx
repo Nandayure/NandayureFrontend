cbb1e75b21bddf86c3717b4136bd25ec889d065d
JosePabloSG <suarezgomezjosepablo03@gmail.com>
2025-02-20 10:53:22 -0600

    Refactor RegisterPage layout and styling; update font usage and enhance design consistency

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,17 +1,16 @@
-import RegisterForm from '@/components/auth/register/register-form';
-import { titleFont } from '@/lib/fonts';
+import RegisterForm from "@/components/auth/register/register-form"
+import { titleFont } from "@/lib/fonts"
 
 export default function RegisterPage() {
   return (
-    <main className="flex items-center justify-center px-4 sm:px-6 lg:px-8 py-12">
-      <div className="w-full border shadow rounded-sm max-w-4xl px-6 py-8">
-        <h5
-          className={`${titleFont.className} text-center text-lg sm:text-2xl font-bold tracking-tight text-gray-900 mb-6`}
-        >
+    <main className="flex items-center justify-center min-h-screen px-4 py-8 bg-gray-50">
+      <div className="w-full max-w-4xl bg-white border shadow-lg rounded-lg p-8">
+        <h1 className={`${titleFont.className} text-center text-2xl font-bold text-gray-900 mb-6`}>
           Registro de usuario
-        </h5>
+        </h1>
         <RegisterForm />
       </div>
     </main>
-  );
+  )
 }
+
